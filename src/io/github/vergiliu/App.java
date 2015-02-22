3cb62c71d3dd0b82e21b5b69ce3e66a5f961f959
vergiliu <vergiliu@gmail.com>
2015-02-22 15:30:30 +0100

    added logging (basic), slf4j

--- src/io/github/vergiliu/App.java
+++ src/io/github/vergiliu/App.java
@@ -1,14 +1,21 @@
 package io.github.vergiliu;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * Hello world!
+ * Project
  *
  */
 
 public class App 
 {
+    static Logger theLogger = LoggerFactory.getLogger("App");
+    
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        theLogger.info("end of file {}", "here");
     }
 }
