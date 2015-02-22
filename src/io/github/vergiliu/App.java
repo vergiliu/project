171b01cd65e1a0b0adf4b6d45967d8242b3dfb5e
vergiliu <vergiliu@gmail.com>
2015-02-22 16:48:24 +0100

    added http async client from apache examples

--- src/io/github/vergiliu/App.java
+++ src/io/github/vergiliu/App.java
@@ -4,6 +4,8 @@ package io.github.vergiliu;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+
 /**
  * Project
  *
@@ -16,6 +18,16 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        GetHTTPData myObject = new GetHTTPData();
+        try {
+            myObject.doAllStuff();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         theLogger.info("end of file {}", "here");
     }
 }
