d74a32f6295438562311d95f2074286a4c5d7c10
vergiliu <vergiliu@gmail.com>
2015-03-01 20:05:15 +0100

    mvn spring-boot:run runs :)

--- src/io/github/vergiliu/App.java
+++ src/io/github/vergiliu/App.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
 
@@ -13,6 +14,7 @@ import java.io.IOException;
  *
  */
 
+@Configuration
 @SpringBootApplication
 public class App 
 {
